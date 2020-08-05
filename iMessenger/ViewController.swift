54708828449e290e4c0bf7624a3a8ef64c9cd9da
Kyaw Zin Htun <kzhtun@gmail.com>
2020-08-05 11:46:37 +0630

    Initial Commit

---
+++ iMessenger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iMessenger
+//
+//  Created by Kyaw Zin Htun on 05/08/2020.
+//  Copyright © 2020 Kyaw Zin Htun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
